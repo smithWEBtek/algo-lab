3d611f475517be263fa7f74b8cf3ae89660f19e0
smithwebtek <brad@smithwebtek.com>
2018-04-06 13:16:18 -0400

    Add binary search

---
+++ binary_search/data/arr42.js
@@ -0,0 +1,10 @@
+
+const evens = [2, 4, 6, 8, 10]
+const odds = [1, 3, 5, 7, 9, 11]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const mix1 = ['a', 'e', 'b', 'h', 'y', 'r', 'd', 'Y', 'E', 'G', 'o', 'u', 'j', 'h']
+const mix2 = [4, 'f', 'h', 4, 't', 3, 'r', 5, 'D', 3, 't', 'q', 4, 'B', 7, 9, 'E']
+const colors = ['red', 'green', 'blue', 'violet', 'yellow', 'orange', 'black', 'turquoise', 'magenta', 'aqua']
+const fruits = ["mango", "apple", "grape", "bananna", "plum", "durian"]
+const pets = ['turtle', 'lobster', 'lemur', 'ferret', 'cat', 'dog', 'fish', 'bird', 'hamster']
---
+++ binary_search/index.js
@@ -0,0 +1,4 @@
+function binary_search(arr, num) {
+	var halfArr = arr.slice(0, arr.length)
+	console.log(halfArr);
+}
